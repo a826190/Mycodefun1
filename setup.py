243f3c37e7ead0ee3ff1115fae9152d6c08f1c72
a826190 <91068205+a826190@users.noreply.github.com>
2021-10-18 20:11:13 +0530

    made function my own

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='custom', version='0.0.1', packages=find_packages(),
-      install_requires=['iotfunctions@git+https://github.com/ibm-watson-iot/functions.git@production'
+      install_requires=['iotfunctions@git+https://github.com/ibm-watson-iot/functions.git@production','pip'
 
 
       ])
