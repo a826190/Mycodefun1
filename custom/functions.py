d9caeabe39e99b0ca7756205aebbcd94e986760c
a826190 <91068205+a826190@users.noreply.github.com>
2021-10-18 19:56:42 +0530

    made function my own

--- custom/functions.py
+++ custom/functions.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 # Specify the URL to your package here.
 # This URL must be accessible via pip install
 
-PACKAGE_URL = 'git+https://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@github.com/a826190/Mycodefun1.git@starter_package --upgrade'
+PACKAGE_URL = '-b Starter_package git+https://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@github.com/a826190/Mycodefun1.git'
 
 
 class HelloWorld_aguptav1(BaseTransformer):
