9468f6ebaed0b0e58ad9efb49665cc40f1022a28
a826190 <91068205+a826190@users.noreply.github.com>
2021-10-18 18:49:36 +0530

    made function my own

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='custom', version='0.0.1', packages=find_packages(),
-      install_requires=[
-            
+      install_requires=['git+git://github.com/ibm-watson-iot/functions@production --upgrade 
+'
+
       ])
